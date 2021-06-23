58fd8b02208db79fe450a92d7104253cada7ca09
Saurabh <ssaurab3@jcp.com>
2021-06-24 01:13:29 +0530

    adding commen ts

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import GameBoard from "./Component/GameBoard";
 function App() {
   const [height, setHeight] = useState(8); // setting up initial table length
   const [width, setwidth] = useState(8);
-  const [mines, setMines] = useState(10); // mine counts
+  const [mines, setMines] = useState(10); // initial mine counts
   return (
     <div className="gameApp">
       <GameBoard height={height} width={width} mines={mines} />
